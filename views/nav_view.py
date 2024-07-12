9245e463b924e6149c6f1bd6656593854d239425
Michael Angelo Berrospi <blckmessiah@protonmail.com>
2024-07-12 15:53:58 +0300

    Added save,undo and clear selection buttons

--- views/nav_view.py
+++ views/nav_view.py
@@ -19,6 +19,39 @@ class NavView(object):
                                 width=2,
                                 style={"padding-top": "3px", "padding-left": "25px"},
                             ),
+                            dbc.Col(
+                                [
+                                    dbc.Button(
+                                        "Save",
+                                        id="save-button",
+                                        color="success",
+                                        style={"margin-right": "20px"},
+                                        disabled=True,
+                                    ),
+                                    dbc.Button(
+                                        "Undo",
+                                        id="undo-button",
+                                        color="secondary",
+                                        style={"margin-right": "20px"},
+                                        disabled=True,
+                                    ),
+                                    dbc.Button(
+                                        "Clear Selection",
+                                        id="clear-button",
+                                        color="warning",
+                                        disabled=True,
+                                    ),
+                                ],
+                                width=2,
+                            ),
+                            dbc.Col(
+                                dbc.Button(
+                                    "Instructions",
+                                    id="instructions_toggle",
+                                    n_clicks=0,
+                                ),
+                                width={"size": 1, "offset": 7},
+                            ),
                         ]
                     )
                 ],
@@ -29,3 +62,7 @@ class NavView(object):
         )
 
         return nav_content
+
+
+if __name__ == "__main__":
+    raise RuntimeError("[ERROR] This module cannot be run like a script.")
