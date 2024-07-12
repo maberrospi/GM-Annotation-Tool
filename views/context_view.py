9634b5e3a7e7d2a048fd4fb523e8adb4e95b7802
Michael Angelo Berrospi <blckmessiah@protonmail.com>
2024-07-12 15:55:54 +0300

    Updated main page content layout and image loading

--- views/context_view.py
+++ views/context_view.py
@@ -7,8 +7,6 @@ import plotly.express as pex
 import cv2
 import os
 
-from GMAnnotationTool import app
-
 
 class ContextView(object):
     def __init__(self) -> None:
@@ -31,7 +29,7 @@ class ContextView(object):
             is_open=self.show_instructions,
             dismissable=True,
             icon="info",
-            style={"position": "fixed", "top": 66, "right": 15, "max-width": 640},
+            style={"position": "fixed", "top": 60, "right": 10, "max-width": 640},
         )
         return instructions_toast
 
@@ -49,27 +47,70 @@ class ContextView(object):
                         "No images in the input folder.",
                         style={"padding": "15px", "margin-bottom": "0px"},
                     ),
-                    dcc.Upload(
-                        id="upload-image",
-                        children=html.Div(
-                            [
-                                "Drag and Drop or ",
-                                html.A("Select Files", style={"font-weight": "bold"}),
-                            ]
-                        ),
-                        style={
-                            "width": "100%",
-                            "height": "60px",
-                            "lineHeight": "60px",
-                            "borderWidth": "1px",
-                            "borderStyle": "dashed",
-                            "borderRadius": "5px",
-                            "textAlign": "center",
-                            "margin": "10px",
-                        },
-                        multiple=True,
+                    dbc.Col(
+                        [
+                            dcc.Upload(
+                                id="upload-image",
+                                children=html.Div(
+                                    [
+                                        "Drag and Drop or ",
+                                        html.A(
+                                            "Select File",
+                                            style={"font-weight": "bold"},
+                                        ),
+                                    ]
+                                ),
+                                style={
+                                    "width": "100%",
+                                    "height": "60px",
+                                    "lineHeight": "60px",
+                                    "borderWidth": "1px",
+                                    "borderStyle": "dashed",
+                                    "borderRadius": "5px",
+                                    "textAlign": "center",
+                                    "margin": "10px",
+                                },
+                                multiple=True,
+                            ),
+                            html.Div(
+                                id="output-image-upload",
+                                style={"maxWidth": "100%", "margin": "10px"},
+                            ),
+                        ],
+                        style={"width": "50%"},
                     ),
-                    html.Div(id="output-image-upload", style={"width": "100%"}),
+                    dbc.Col(
+                        [
+                            dcc.Upload(
+                                id="upload-image-two",
+                                children=html.Div(
+                                    [
+                                        "Drag and Drop or ",
+                                        html.A(
+                                            "Select File",
+                                            style={"font-weight": "bold"},
+                                        ),
+                                    ]
+                                ),
+                                style={
+                                    "width": "100%",
+                                    "height": "60px",
+                                    "lineHeight": "60px",
+                                    "borderWidth": "1px",
+                                    "borderStyle": "dashed",
+                                    "borderRadius": "5px",
+                                    "textAlign": "center",
+                                    "margin": "10px",
+                                },
+                                multiple=True,
+                            ),
+                            html.Div(
+                                id="output-image-upload-two",
+                                style={"maxWidth": "100%", "margin": "10px"},
+                            ),
+                        ],
+                        style={"width": "50%"},
+                    ),
                 ],
                 style={"width": "100%"},
             )
@@ -86,7 +127,7 @@ class ContextView(object):
                 )
             ],
             header=header,
-            style={"width": "100%", "maxWidth": "900px", "margin-left": "10px"},
+            style={"width": "100%", "maxWidth": "100%", "margin-left": "10px"},
             body_style={"padding": "0px 0px 0px 0px", "margin": "0px 0px 0px 0px"},
         )
 
@@ -96,14 +137,15 @@ class ContextView(object):
         matches_content_toast = dbc.Toast(
             [
                 dbc.ListGroup(
-                    id="matched-points",
+                    id="matched_points",
                     children=[],
                     style={"border": "0px", "border-radius": "0%"},
                     className="mb-0",
+                    numbered=True,
                 ),
             ],
             header="Matched Points",
-            style={"width": "400px", "maxWidth": "400px"},
+            style={"width": "400px", "maxWidth": "400px", "minWidth": "400px"},
             body_style={"padding": "0px 0px 0px 0px", "margin": "0px 0px 0px 0px"},
         )
         return matches_content_toast
@@ -118,20 +160,18 @@ class ContextView(object):
             children=[
                 dbc.Row(
                     [
-                        dbc.Col(display_content_toast, style={"padding-right": "0px"}),
-                        dbc.Col(matches_content_toast, style={"padding-right": "0px"}),
+                        dbc.Col(
+                            display_content_toast,
+                            style={"padding-right": "0px", "maxWidth": "100%"},
+                            width=7,
+                        ),
+                        dbc.Col(
+                            matches_content_toast,
+                            style={"padding-right": "0px"},
+                            width="auto",
+                        ),
                     ],
                     class_name="mb-3",
-                ),
-                dbc.Button(
-                    "Instructions",
-                    id="instructions_toggle",
-                    n_clicks=0,
-                    style={
-                        "position": "fixed",
-                        "top": 66,
-                        "right": 10,
-                    },
                 ),
                 # generate instructions here
                 instructions_toast,
@@ -140,3 +180,7 @@ class ContextView(object):
         )
 
         return content
+
+
+if __name__ == "__main__":
+    raise RuntimeError("[ERROR] This module cannot be run like a script.")
