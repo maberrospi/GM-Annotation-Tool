54eb6120c1e4019bbd8d3e733e696780d8b716bb
Michael Angelo Berrospi <blckmessiah@protonmail.com>
2024-07-16 16:56:54 +0200

    Added undo and save functionalities

--- GMAnnotationTool.py
+++ GMAnnotationTool.py
@@ -1,4 +1,4 @@
-from dash import Dash, html, dcc, ctx
+from dash import Dash, html, dcc
 from dash.exceptions import PreventUpdate
 import dash
 import dash_bootstrap_components as dbc
@@ -8,8 +8,10 @@ import numpy as np
 import cv2
 from PIL import Image
 import io
+import os
 import base64
 import inspect
+import json
 from pathlib import Path
 
 import functionalities.annotator
@@ -18,9 +20,7 @@ import homepage
 app = Dash(__name__, suppress_callback_exceptions=True)
 app.config.external_stylesheets = [dbc.themes.BOOTSTRAP]
 
-# app.layout = [html.Div(children="Hello World")]
-
-# Create functionality objects
+# Create annotation functionality objects
 left_annotator = functionalities.annotator.MatchAnnotator(name="left")
 right_annotator = functionalities.annotator.MatchAnnotator(name="right")
 
@@ -30,21 +30,12 @@ CWD_PATH = Path.cwd()
 
 @app.callback(
     dash.dependencies.Output("page-content", "children"),
-    dash.dependencies.Input("save-button", "n_clicks"),
     dash.dependencies.Input("url", "pathname"),
 )
-def display_page(n_clicks, pathname):
-    # Click on the submit button
-    if n_clicks is not None:
-        return homepage.HomePage().generate_context()
-
+def display_page(pathname):
     # HTTP GET of any URL
     if pathname is not None:
         return homepage.HomePage().generate_context()
-        if pathname == "/instructions":
-            pass
-        else:
-            pass
 
 
 @app.callback(
@@ -52,7 +43,7 @@ def display_page(n_clicks, pathname):
     dash.dependencies.Input("instructions_toggle", "n_clicks"),
 )
 def display_instructions(n_clicks):
-    return True if n_clicks else False
+    return True if n_clicks or n_clicks > 0 else False
 
 
 @app.callback(
@@ -113,8 +104,6 @@ def parse_contents(contents, filename, date, f_name):
         graph_div_id = "right_graph_div"
         graph_id = "right_graph"
 
-    # Plot image
-    # img = resized[..., ::-1].copy()
     img = resized.copy()
 
     # Convert the image string to numpy array and create a
@@ -167,7 +156,6 @@ def parse_contents(contents, filename, date, f_name):
         config=config,
         style={"height": "100%", "width": "100%"},
     )
-
     return html.Div(
         [
             html.H5(filename),
@@ -175,13 +163,6 @@ def parse_contents(contents, filename, date, f_name):
             # HTML images accept base64 encoded strings in the same format
             # that is supplied by the upload
             graph,
-            # html.Img(src=contents, style={"height": "100%", "width": "100%"}),
-            # html.Hr(),
-            # html.Div("Raw Content"),
-            # html.Pre(
-            #     contents[0:200] + "...",
-            #     style={"whiteSpace": "pre-wrap", "wordBreak": "break-all"},
-            # ),
         ],
         id=graph_div_id,
     )
@@ -195,7 +176,6 @@ def parse_contents(contents, filename, date, f_name):
 )
 def update_image(list_of_contents, list_of_names, list_of_dates):
     if list_of_contents is not None:
-        # self.path = False
         children = [
             parse_contents(c, n, d, str(inspect.stack()[0][3]))
             for c, n, d in zip(list_of_contents, list_of_names, list_of_dates)
@@ -211,7 +191,6 @@ def update_image(list_of_contents, list_of_names, list_of_dates):
 )
 def update_image_two(list_of_contents, list_of_names, list_of_dates):
     if list_of_contents is not None:
-        # self.path = False
         children = [
             parse_contents(c, n, d, str(inspect.stack()[0][3]))
             for c, n, d in zip(list_of_contents, list_of_names, list_of_dates)
@@ -219,7 +198,9 @@ def update_image_two(list_of_contents, list_of_names, list_of_dates):
         return children
 
 
-def click_event_handler(clickData, annotator, fig, matched_points: list):
+def click_event_handler(
+    clickData, annotator, fig, matched_points: list
+):  # , line_g_hover, vis_pts
     # Click on the graph canvas
     # Pass clicks to the annotator
     points = clickData.get("points")[0]
@@ -228,6 +209,12 @@ def click_event_handler(clickData, annotator, fig, matched_points: list):
     idx = len(annotator.clicks)
     annotator.add_click(x, y)
     print(annotator, annotator.clicks)
+
+    # Add points to store for line visuaization
+    # line_x = line_g_hover["points"][0]["x"]
+    # line_y = line_g_hover["points"][0]["y"]
+    # vis_pts.append((line_x, line_y))
+    # print(vis_pts)
 
     fig = go.Figure(fig)
     # Update the figure with the new annotation
@@ -250,51 +237,119 @@ def click_event_handler(clickData, annotator, fig, matched_points: list):
             matched_points[-1]["props"]["children"] + f" -> x = {x}, y = {y}"
         )
 
-    return fig, matched_points
+    return fig, matched_points  # , vis_pts
 
 
 @app.callback(
     dash.dependencies.Output("left_graph", "figure", allow_duplicate=True),
     dash.dependencies.Output("matched_points", "children", allow_duplicate=True),
+    dash.dependencies.Output("store-last-annot", "data", allow_duplicate=True),
     dash.dependencies.Input("left_graph", "clickData"),
     dash.dependencies.State("left_graph", "figure"),
     dash.dependencies.State("matched_points", "children"),
+    # dash.dependencies.State("line-graph", "hoverData"),
+    # dash.dependencies.State("points-store-1", "data"),
     prevent_initial_call=True,
 )
-def handle_left_click(clickData, fig, matched_points):
+def handle_left_click(clickData, fig, matched_points):  # , line_g_hover, vis_pts_1
+    # print(line_g_hover)
     if not clickData or len(left_annotator.clicks) > len(right_annotator.clicks):
         raise PreventUpdate
     else:
         # print(fig["data"])
         figure, matched_points_view = click_event_handler(
             clickData, left_annotator, fig, matched_points
-        )
+        )  # , line_g_hover, vis_pts_1
+        # Update the last annotated stored value
+        store_last_annot = "left"
 
-        return figure, matched_points_view
+        return figure, matched_points_view, store_last_annot  # , vis_pts
 
 
 @app.callback(
     dash.dependencies.Output("right_graph", "figure", allow_duplicate=True),
     dash.dependencies.Output("matched_points", "children", allow_duplicate=True),
+    dash.dependencies.Output("store-last-annot", "data", allow_duplicate=True),
     dash.dependencies.Input("right_graph", "clickData"),
     dash.dependencies.State("right_graph", "figure"),
     dash.dependencies.State("matched_points", "children"),
+    # dash.dependencies.State("line-graph", "hoverData"),
+    # dash.dependencies.State("points-store-2", "data"),
     prevent_initial_call=True,
 )
-def handle_right_click(clickData, fig, matched_points):
+def handle_right_click(clickData, fig, matched_points):  # , line_g_hover, vis_pts_2
     if not clickData or len(left_annotator.clicks) <= len(right_annotator.clicks):
         raise PreventUpdate
     else:
         # print(fig["data"])
         figure, matched_points_view = click_event_handler(
             clickData, right_annotator, fig, matched_points
-        )
+        )  # , line_g_hover, vis_pts_2
+        # Update the last annotated stored value
+        store_last_annot = "right"
 
-        return figure, matched_points_view
+        return figure, matched_points_view, store_last_annot  # , vis_pts
 
 
-def undo_event_handler(undo_n_clicks):
-    pass
+# @app.callback(
+#     dash.dependencies.Output('background', 'figure'),
+#     dash.dependencies.Input("right_graph", "clickData"),
+#     dash.dependencies.State('left-image', 'figure'),
+#     dash.dependencies.State('right-image', 'figure')
+# )
+
+# def draw_match_line(clickData,left_fig,right_fig):
+# Determine the full x range across both images
+#     left_x_range = left_figure['layout']['xaxis']['range']
+# right_x_range = right_figure['layout']['xaxis']['range']
+# full_x_range = [left_x_range[0], right_x_range[1]]
+
+
+@app.callback(
+    dash.dependencies.Output("matched_points", "children", allow_duplicate=True),
+    dash.dependencies.Output("left_graph", "figure", allow_duplicate=True),
+    dash.dependencies.Output("right_graph", "figure", allow_duplicate=True),
+    dash.dependencies.Output("store-last-annot", "data", allow_duplicate=True),
+    dash.dependencies.Input("undo-button", "n_clicks"),
+    dash.dependencies.State("left_graph", "figure"),
+    dash.dependencies.State("right_graph", "figure"),
+    dash.dependencies.State("store-last-annot", "data"),
+    prevent_initial_call=True,
+)
+def undo_event_handler(undo_n_clicks, l_fig, r_fig, last_annotation):
+    if undo_n_clicks:
+        if last_annotation == "left":
+            updated_last_annot = "right"
+            # Update figures
+            l_fig["data"].pop()
+            # Update annotation points
+            left_annotator.clicks.pop()
+        elif last_annotation == "right":
+            updated_last_annot = "left"
+            # Update figures
+            r_fig["data"].pop()
+            # Update annotation points
+            right_annotator.clicks.pop()
+
+        updated_matched_points = []
+
+        for points in zip(left_annotator.clicks, right_annotator.clicks):
+            left_point, right_point = points[0], points[1]
+            new_point = dbc.ListGroupItem(
+                f"x = {left_point['x']}, y = {left_point['y']} -> x = {right_point['x']}, y = {right_point['y']}"
+            )
+            updated_matched_points.append(new_point)
+
+        if len(left_annotator.clicks) != len(right_annotator.clicks):
+            # The last point from the left annotator was skipped so add it
+            new_point = dbc.ListGroupItem(
+                f"x = {left_annotator.clicks[-1]['x']}, y = {left_annotator.clicks[-1]['y']}"
+            )
+            updated_matched_points.append(new_point)
+
+        return updated_matched_points, l_fig, r_fig, updated_last_annot
+    else:
+        raise PreventUpdate
 
 
 @app.callback(
@@ -326,8 +381,68 @@ def clear_selection_handler(clear_n_clicks, l_fig, r_fig):
         raise PreventUpdate
 
 
-def save_event_handler(save_n_clicks):
-    pass
+def create_JSON_annotation():
+    real_clicks = []
+
+    for clicks in zip(left_annotator.clicks, right_annotator.clicks):
+        left_clks = clicks[0]
+        right_clks = clicks[1]
+        left_scale_factor = left_annotator.scale_factor
+        right_scale_factor = right_annotator.scale_factor
+        real_clicks.append(
+            {
+                "x1": int(round(left_clks["x"] / left_scale_factor)),
+                "y1": int(round(left_clks["y"] / left_scale_factor)),
+                "x2": int(round(right_clks["x"] / right_scale_factor)),
+                "y2": int(round(right_clks["y"] / right_scale_factor)),
+            }
+        )
+
+    return {"matches": real_clicks}
+
+
+@app.callback(
+    dash.dependencies.Output("save-modal", "is_open"),
+    dash.dependencies.Output("page-content", "children", allow_duplicate=True),
+    dash.dependencies.Output("save-button", "n_clicks"),
+    dash.dependencies.Output("success-pop-up", "is_open"),
+    dash.dependencies.Input("save-button", "n_clicks"),
+    dash.dependencies.Input("close-message", "n_clicks"),
+    dash.dependencies.State("save-modal", "is_open"),
+    prevent_initial_call=True,
+)
+def save_event_handler(save_n_clicks, close_n_clicks, modal_is_open):
+    # First time running it
+    if save_n_clicks is None:
+        raise PreventUpdate
+
+    changed_btn_id = [b["prop_id"] for b in dash.ctx.triggered][0]
+
+    if changed_btn_id == "close-message.n_clicks":
+        return not modal_is_open, dash.no_update, save_n_clicks, dash.no_update
+    elif changed_btn_id == "save-button.n_clicks":
+
+        l_annot_n_clicks = len(left_annotator.clicks)
+        r_annot_n_clicks = len(right_annotator.clicks)
+        if l_annot_n_clicks != r_annot_n_clicks:
+            print(not modal_is_open)
+            return not modal_is_open, dash.no_update, 0, dash.no_update
+
+        im1_path = left_annotator.img_path.split(".")[0]
+        im2_path = right_annotator.img_path.split(".")[0]
+        json_fname = im1_path + im2_path + ".json"
+        output_dir = left_annotator.output_dir  # Left dir == right dir
+        dst_path = os.path.join(CWD_PATH, output_dir, json_fname)
+        Path(dst_path).parent.mkdir(parents=True, exist_ok=True)
+        json_dict = create_JSON_annotation()
+        with open(dst_path, "w") as json_file:
+            json.dump(json_dict, json_file)
+
+        # If save succesfully reset annotations for next pair
+        left_annotator.reset_all_annotations()
+        right_annotator.reset_all_annotations()
+
+        return modal_is_open, homepage.HomePage().generate_context(), 0, True
 
 
 def main():
@@ -338,25 +453,12 @@ def main():
             dcc.Location(id="url", refresh=False),
             html.Div(
                 id="page-content",
-                children=[
-                    html.Div(
-                        id="submit-button-hidden-div",
-                        style={"display": "none"},
-                        children=[
-                            dbc.Button(
-                                "Submit",
-                                id="save-button",
-                                color="success",
-                            ),
-                        ],
-                    ),  # Used for the callback, otherwise the page would not load
-                    # as a button has to be created for the callback to work
-                ],
+                children=[],
             ),
         ]
     )
 
-    app.run(debug=True)
+    app.run(debug=False)
 
 
 if __name__ == "__main__":
